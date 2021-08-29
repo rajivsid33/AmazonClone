069d8d759b9ef62bfe49e8c0489e67d22319f126
rajivsid33 <atsrajiv@gmail.com>
2021-08-29 17:55:32 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Header from "./Header";
+import Home from "./Homes/Home";
+import CheckOutPage from './CheckOutPage/CheckOutPage';
+
+
+
+function App() {
+
+  return (
+    <Router>
+    <>
+      <Switch>
+     
+        <Route exact path="/checkout">
+          <Header/> 
+           <CheckOutPage/>
+
+        </Route>
+        <Route exact path='/about'>
+          <Header/>
+          <h1>hello petre</h1>
+        </Route>
+        <Route >
+          <Header/>
+          <Home/>     
+        </Route>
+      </Switch>
+    </>
+    </Router>
+  )
+}
+export default App;
